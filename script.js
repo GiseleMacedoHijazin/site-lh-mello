09c0c4e3254297bcdb775e3520b666fcedf90106
Pedro Papini <ppapinia@gmail.com>
2022-07-31 15:19:51 -0300

    Passa JS para arquivo separado

---
+++ script.js
@@ -0,0 +1,43 @@
+const video = document.getElementById('video');
+video.playbackRate = 0.4;
+
+const contactForm = document.getElementById('contact-form');
+const submitBtn = document.getElementById('submit-btn');
+
+formChange = () => {
+  const name = document.getElementById('input-name').value;
+  const email = document.getElementById('input-email').value;
+  const message = document.getElementById('input-message').value;
+
+  const emailRegex = /\w+@\w+.com/;
+  const validEmail = emailRegex.test(email);
+
+  submitBtn.disabled = !(name.length > 0 && validEmail && message.length > 0);
+}
+
+contactForm.addEventListener('keyup', formChange);
+submitBtn.addEventListener('click', (event) => {
+  alert('Mensagem enviada!')
+});
+
+const mobileMenuIcon = document.getElementById('mobile-menu-icon');
+const menu = document.getElementById('nav-bar')
+
+mobileMenuIcon.addEventListener('click', () => {
+  if (menu.classList.contains('move-in')) {
+    menu.classList.remove('move-in');
+    menu.classList.add('move-out');
+  } else {
+    menu.classList.remove('move-out');
+    menu.classList.add('move-in');
+  }
+  if (mobileMenuIcon.classList.contains('menu-open')) {
+    mobileMenuIcon.classList.remove('menu-open');
+    mobileMenuIcon.classList.add('menu-close');
+    mobileMenuIcon.innerHTML = '&#x2630;';
+  } else {
+    mobileMenuIcon.classList.add('menu-open');
+    mobileMenuIcon.classList.remove('menu-close');
+    mobileMenuIcon.innerHTML = '&#x2715;';
+  }
+});
